1f0a7720f3952da399b68d2f0684f8b3175a45cf
Mike Schennum <mikeschenphoto@gmail.com>
2024-08-29 11:01:50 -0700

    add in 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,9 +16,8 @@ const router = createRouter({
       component: () => import('../views/LoginView.vue')
     },
     {
-      path: '/exchange_token',
-      name: 'home',
-      component: HomeView
+      path: '/:pathMatch(.*)*',
+      redirect: '/'
     }
   ]
 });
