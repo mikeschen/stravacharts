84800d394943fdd1aacdcf0b53950901046f7408
Mike Schennum <mikeschenphoto@gmail.com>
2024-08-29 10:29:45 -0700

    add prod redirect url

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -12,7 +12,10 @@
 <script setup lang="ts">
 const login = () => {
   const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-  const redirectUrl = 'http://localhost:5173';
-  window.location = `http://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=read,profile:read_all,activity:read,activity:read_all` as unknown as Location;
+  const redirectUrl =
+    process.env.NODE_ENV === 'production'
+      ? 'https://stravacharts.vercel.app'
+      : 'http://localhost:5173';
+  window.location = `http://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=read,profile:read_all,activity:read,activity:read_all`;
 };
 </script>
