01777e6c0ae586ff2419f467b6064e8b8eeefed7
Mike Schennum <mikeschenphoto@gmail.com>
2024-09-06 12:00:58 -0700

    update URL for redirect

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -91,12 +91,10 @@ const { token, athlete } = storeToRefs(authStore);
 const login = () => {
   const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
   const redirectUrl =
-    process.env.NODE_ENV === 'production'
-      ? 'https://scharts.vercel.app'
-      : 'http://localhost:5173';
-  const url = `http://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=read,profile:read_all,activity:read,activity:read_all`;
+    process.env.NODE_ENV === 'production' ? 'https://scharts.vercel.app' : 'http://localhost:5173';
+  const url = `https://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=read,profile:read_all,activity:read,activity:read_all`;
 
-  window.location.href = url;
+  window.location = url;
 };
 
 const logout = () => {
